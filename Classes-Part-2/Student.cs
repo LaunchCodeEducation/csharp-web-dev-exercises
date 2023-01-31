00e4c518341e99caee4d96f742b56c1286dee57d
Courtney Frey <courtneyb.frey@gmail.com>
2023-01-31 11:07:17 -0600

    starter code added for classes part 2 lesson

---
+++ Classes-Part-2/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Classes_Part_2
+{
+	public class Student
+	{
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+        public Student(string name, int studentId,
+            int numberOfCredits, double gpa)
+        {
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+
+        public Student(string name, int studentId)
+        : this(name, studentId, 0, 0) { }
+
+        public Student(string name)
+        : this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
+
+        // TODO: Complete the AddGrade method.
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // Update the appropriate properties: NumberOfCredits, Gpa
+        }
+
+        //TODO: Complete the GetGradeLevel method here:
+        public string GetGradeLevel(int credits)
+        {
+            // Determine the grade level of the student based on NumberOfCredits
+            return "grade level tbd";
+        }
+
+        // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
+        //  than just the class fields.
+
+        // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
+        //  Student objects equal.
+    }
+}
+
